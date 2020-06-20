decadb6f8cd8d2db645b22bd3625b20514ff846d
basemkhirat <basemkhirat@gmail.com>
2020-06-21 01:36:58 +0200

    add passesAsync and failsAsync for validation service

--- services/validator/index.js
+++ services/validator/index.js
@@ -1,20 +1,40 @@
 const Validator = require("validatorjs");
 
-
 module.exports = class extends Validator {
+	/**
+	 * Create the validator object
+	 * @param  {...any} params
+	 */
+	static make(...params) {
+		return new this(...params);
+	}
 
-    /**
-     * Promise based validator method
-     * @return promise
-     */
-    validate() {
-        return new Promise((resolve, reject) => {
-            if (this.hasAsync) {
-                this.passes(() => resolve(true));
-                this.fails(() => resolve(false));
-            } else {
-                this.passes() ? resolve(true) : resolve(false);
-            }
-        });
-    }
-}
+	/**
+	 * Promise based validator method
+	 * @return promise
+	 */
+	validate() {
+		return new Promise((resolve, reject) => {
+			if (this.hasAsync) {
+				this.passes(() => resolve(true));
+				this.fails(() => resolve(false));
+			} else {
+				this.passes() ? resolve(true) : resolve(false);
+			}
+		});
+	}
+
+	/**
+	 * check async valid operation
+	 */
+	async passesAsync() {
+		return await this.validate();
+	}
+
+	/**
+	 * check async invalid operation
+	 */
+	async failsAsync() {
+		return !(await this.validate());
+	}
+};
