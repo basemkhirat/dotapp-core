ef2d9ddd640cede03d1b65b5eefb03fc1bb529e3
basemkhirat <basemkhirat@gmail.com>
2020-06-21 03:57:30 +0200

    validation json files

--- middlewares/i18n.js
+++ middlewares/i18n.js
@@ -1,54 +1,50 @@
-const I18n = require('i18n');
-const Config = require('dotapp/services/config');
+const I18n = require("i18n");
+const Config = require("dotapp/services/config");
 const moment = require("moment");
-const parser = require('accept-language-parser');
+const parser = require("accept-language-parser");
 const Validator = require("validatorjs");
 const path = require("path");
+const fs = require("fs");
 
 module.exports = function () {
+	let config = Config.get("i18n");
 
-    let config = Config.get("i18n");
+	I18n.configure(config);
 
-    I18n.configure(config);
+	return function (req, res, next) {
+		let default_locale = config.defaultLocale;
 
-    return function (req, res, next) {
+		if (req.headers["accept-language"] !== undefined) {
+			let header_lang = req.headers["accept-language"];
 
-        let default_locale = config.defaultLocale;
+			if (config.locales.indexOf(header_lang) > -1) {
+				default_locale = header_lang;
+			} else {
+				let languages = parser.parse(header_lang);
 
-        if (req.headers["accept-language"] !== undefined) {
+				if (languages.length) {
+					default_locale = languages[0].code;
+				}
+			}
+		} else {
+			if (req.user && config.locales.indexOf(req.user.lang) > -1) {
+				default_locale = req.user.lang;
+			}
+		}
 
-            let header_lang = req.headers["accept-language"];
+		req.language = default_locale;
 
-            if (config.locales.indexOf(header_lang) > -1) {
+		moment.locale(default_locale);
 
-                default_locale = header_lang;
+		Validator.useLang(req.getLocale());
 
-            } else {
-
-                let languages = parser.parse(header_lang);
-
-                if (languages.length) {
-                    default_locale = languages[0].code;
-                }
-            }
-
-        } else {
-
-            if (req.user && config.locales.indexOf(req.user.lang) > -1) {
-                default_locale = req.user.lang;
-            }
-
-        }
-
-        req.language = default_locale;
-
-        moment.locale(default_locale);
-
-
-        Validator.useLang(req.getLocale());
-
-        Validator.setMessages(req.getLocale(), require(path.join(process.cwd(), "lang/" + req.getLocale() + "/validation")));
-
-        next();
-    }
-}
+		fs.readFile(
+			path.join(process.cwd(), "lang/" + req.getLocale() + "/validation.json"),
+			(error, data) => {
+				if (error) return next(error);
+				Validator.setMessages(req.getLocale(), JSON.parse(data));
+				next();
+			}
+		);
+	};
+};
